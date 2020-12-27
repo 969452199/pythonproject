6fa0490dc9c271be004d480d5445205c04dc4415
969452199 <969452199@qq.com>
2020-12-27 11:10:36 +0800

    'taobao'

---
+++ test1.py
@@ -0,0 +1,17 @@
+1.创建一个新项目，按操作执行
+
+2.更新代码想要提交，步骤：
+git status查看仓库代码状态，更新的什么
+git add . ,编译文件，若报错，再编译一次，直到没有任何提示
+git commit -m “备注内容”
+git push 提交到远程分支master
+
+
+在公司更新了代码，在家里也需要用时，用git pull远程新的代码，更新到本地
+
+
+
+每次更新代码，git status 查看仓库代码状态，更新的什么
+git add .  编译文件，再进行一次git add .
+git commit -m "备注"
+git push 提交代码
