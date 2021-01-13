4ebece5be5e23896433416ee550e396435853651
969452199 <969452199@qq.com>
2021-01-14 00:35:07 +0800

    add comment

--- day11/unittest02.py
+++ day11/unittest02.py
@@ -2,6 +2,6 @@ import time
 import unittest
 #加载当前目录，与unittest02.py在同一个目录(一个.是当前目录；..是上级目录）
 test_dir = './'
-#加载当前目录下以class开头的.PY文件
+#加载当前目录下以class开头的.PY文件11
 discover = unittest.defaultTestLoader.discover(test_dir,pattern='class*.py')
 unittest.TextTestRunner().run(discover)
