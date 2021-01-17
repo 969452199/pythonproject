b0b073847966a625e90cdb4f02957708ba00471e
969452199 <969452199@qq.com>
2021-01-17 09:13:46 +0800

    add test

--- day11/unittest02.py
+++ day11/unittest02.py
@@ -1,7 +1,7 @@
 import time
 import unittest
-#加载当前目录，与unittest02.py在同一个目录(一个.是当前目录；..是上级目录）
-test_dir = './'
-#加载当前目录下以class开头的.PY文件11
-discover = unittest.defaultTestLoader.discover(test_dir,pattern='class*.py')
-unittest.TextTestRunner().run(discover)
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://www.lagou.com/')
+driver.maximize_window()
+driver.implicitly_wait(20)
--- day11/unittest03.py
+++ day11/unittest03.py
@@ -4,7 +4,7 @@ import unittest
 #加载当前目录，与unittest02.py在同一个目录(一个.是当前目录；..是上级目录）
 test_dir = './'
 #加载当前目录下以class开头的.PY文件
-discover = unittest.defaultTestLoader.discover(test_dir,pattern='LaGouLogin.py')
+discover = unittest.defaultTestLoader.discover(test_dir,pattern='LaGouLogin_TEST.py')
 
 #定义报告目录
 file_dir = './Report/'
